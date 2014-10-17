828a2ba2ac315157b17428b19cfa03a4623c5a5f
Andri Freysson <andrifreysson@andris-mbp-3.vodafone>
2014-10-17 15:28:26 +0000

    Testing unknow arguments

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,9 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+     @Test
+    public void testUnknownArguments(){
+    	assertEquals(12, Calculator.add("1,2,1,2,1,2,1,2"));
+    }
 
 }
